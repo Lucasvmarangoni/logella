fb4aa8d650b36b13919eca8c1ad29af7e713bfdf
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2024-02-20 17:53:04 -0300

    ✬feat: add FailOnErrLog

--- err/custom.go
+++ err/custom.go
@@ -1,6 +1,9 @@
 package errors
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+)
 
 func IsRequiredError(fieldName, msg string) error {
 	return fmt.Errorf("%s is required. %s", fieldName, msg)
@@ -13,5 +16,11 @@ func IsInvalidError(fieldName, msg string) error {
 func FailOnErrPanic(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func FailOnErrLog(err error, msg string) {
+	if err != nil {
+		log.Fatal().Err(ErrCtx(err, msg))
 	}
 }
