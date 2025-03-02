9db84e823963aa0ff80f8e29942939af3714378c
lucasvmarangoni <lucasvm.ti@gmail.com>
2025-03-02 17:59:45 -0300

    ✬ test: add log test to errs tests

--- err/test/errs_test.go
+++ err/test/errs_test.go
@@ -1,11 +1,14 @@
 package errs_test
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 
 	errs "github.com/Lucasvmarangoni/logella/err"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,7 +31,12 @@ func service(code int) (string, error) {
 }
 
 func repository(code int) error {
-	return errs.Wrap(errors.New(cause), code).Msg(message)
+	err := externalMethod()
+	return errs.Wrap(err, code).Msg(message)
+}
+
+func externalMethod() error {
+	return errors.New(cause)
 }
 
 func TestErrorOutput(t *testing.T) {
@@ -83,5 +91,39 @@ func TestErrorOutput(t *testing.T) {
 		assert.Equal(t, expectedMessage, errMessage,
 			"Expected error message %d, but got %d. This suggests an issue with the error message returned by the handler.",
 			expectedMessage, errMessage)
+	})
+}
+
+func TestLogOutput(t *testing.T) {
+	t.Run("(Stack) Log should have the same error message, error code, path and trace", func(t *testing.T) {
+		expectedCode := http.StatusInternalServerError
+		_, err := handler(expectedCode)
+
+		var buf bytes.Buffer
+		log.Logger = zerolog.New(&buf)
+
+		log.Error().Err(errs.Unwrap(err).Stack()).Msg("example error")
+		logOutput := buf.String()
+
+		assert.Contains(t, logOutput, `example error`)
+		assert.Contains(t, logOutput, cause)
+		assert.Contains(t, logOutput, `path errs_test.go:35 trace repository ➤ service ➤ handler","message":"example error"`)
+	})
+
+	t.Run("(ToClient) Log should have the same error message and 'Internal Server Error' cause and not have the real cause, path and trace", func(t *testing.T) {
+		expectedCode := http.StatusInternalServerError
+		_, err := handler(expectedCode)
+
+		var buf bytes.Buffer
+		log.Logger = zerolog.New(&buf)
+
+		log.Error().Err(errs.Unwrap(err).ToClient()).Msg("example error")
+		logOutput := buf.String()
+
+		assert.Contains(t, logOutput, `example error`)
+		assert.Contains(t, logOutput, http.StatusText(expectedCode))
+
+		assert.NotContains(t, logOutput, cause)
+		assert.NotContains(t, logOutput, `path errs_test.go:35 trace repository ➤ service ➤ handler","message":"example error"`)
 	})
 }
